ee9bdf446e2a59966568d0a347865101a60f55e2
HOUYUSTON <34230358+HOUYUSTON@users.noreply.github.com>
2018-11-04 15:24:39 +0200

    Lab3
    Lab3

--- index.php
+++ index.php
@@ -1,100 +1,38 @@
 <?php
 	session_start();
 ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="windows-1251">
 	</head>
 	<body>
-		<script type="text/javascript">
-		window.onload = function(){
-			document.getElementById('login').onsubmit=function() {
-				var inputs = document.getElementById("login").elements;
-				$.ajax({
-					type: "POST",
-					url: "login.php",
-					data: {login:inputs[0].value, pass:inputs[1].value},
-					success: function(){
-						alert("Logged in");
-						window.location.href = "profile.php";
-					}
-				})
-				return false;
-			}
-		}
-
-		function search() {
-				var inputs = document.getElementById("search").elements;
-				$.ajax({
-					type: "POST",
-					url: "table.php",
-					data: {login:inputs[0].value},
-					success: function(data){
-						$("#table").html(data);
-					}
-				})
-				return false;
-		}
-
-		function get_table(){
-			$.ajax({
-				url: "table.php",
-				dataType: "html",
-				success: function(data){
-					$("#table").html(data);
-				}
-			})
-		}
-
-		function get_sorted(type){
-			$.ajax({
-				type: "POST",
-				url: "table.php",
-				data: {type:type},
-				success: function(data){
-						$("#table").html(data);
-				}
-			})
-		}
-		</script>
 		<?php
-			if(isset($_SESSION["role"]))
+			include ("db.php");
+			$result=mysqli_query($db,"SELECT * FROM lab3");
+			$props=array();
+			while($object=mysqli_fetch_object($result))
 			{
-				echo "you are ".$_SESSION["role"];
+				$props[]=$object;
 			}
-			else
-			{
-				?>
-				<form id="login">
-				Login: <br> <input type="text" name="login"> <br>
-				Pasword: <br> <input type="password" name="pass"> <br>
-				<input type="submit" value="Enter"> <br>
-				</form>
-				<?php
-			}
-			?>
-			<form action="http://localhost/profile.php">
-			<?php
-			if(isset($_SESSION["login"]))
-    				echo"<input type=\"submit\" value=\"Profile\">";
-		  ?>
-			</form>
-			<br><br><center><form id="search"><input type="text" name="login" id="log_search"><input type="button" value="Search" onClick="search()"><br></form></center><br>
-			<center><input type="button" value="Descending" onclick="get_sorted('desc')"><input type="button" value="Ascending" onclick="get_sorted('asc')"></center>
-			<script> get_table() </script>
-			<center><div id="table"></div></center>
-			<?php
-			if(isset($_SESSION["login"]))
-			{
-				?>
-				<form action="http://localhost/log_out.php">
-				<input type="submit" value="Log out">
-				</form>
-				<?php
+			foreach ($props as $props) {
+				if($props->type=='pic'){
+					echo"<img src=\"props/$props->name\">";
+					echo"<br>";
+				}
+				elseif ($props->type=='vid') {
+					echo"<video src=\"props/$props->name\" controls></video>";
+					echo"<br>";
+				}
+				elseif ($props->type=='src') {
+					echo"<iframe src=\"$props->name\" allowfullscreen=\"\" frameborder=\"0\"></iframe>";
+					echo"<br>";
+				}
+				elseif ($props->type=='mus') {
+					echo"<audio src=\"props/$props->name\" controls>";
+					echo"<br>";
+				}
 			}
 		?>
-
 	</body>
 </html>
